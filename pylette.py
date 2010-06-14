59e8f08e6b68657fb5d6a88f56c440148f66de5c
Nathan Chowning <nathan@nathanchowning.com>
2010-06-14 04:01:13 -0400

    Completed v0.1. Generates 16 shades of a given color.

--- pylette.py
+++ pylette.py
@@ -49,12 +49,98 @@ def flarg(argv, rgb):
 
 # --help function
 def usage():
-    print "Quit being a faggot"
+    print "****************"
+    print "* Pylette Help *"
+    print "****************\n"
+    print "To generate 16 shades of a single color:"
+    print "\tpylette.py RRR GGG BBB\n"
+    print "To generate 16 shades of multiple colors:"
+    print "\tpylette.py --count 16\n"
+
+# Shade creation function.  This function takes the shade given to it (in RRR GGG BBB format) and
+# generates 15 additional shades based on how light/dark the given color is
+def shades(rgb):
+    valmin = 0 # General minimum value
+    valmax = 255 # General maximum value
+
+    # Generate an average of the RGB values (to determine, generally, how light/dark the shade is)
+    divav = (int(rgb[0]) + int(rgb[1]) + int(rgb[2])) / 3
+
+    # This part is dumb and I need to come up with a better way of doing this.
+    # My equations require arrays of a predetermined size (since this was written in c++ originally)
+    redar = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    greenar = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    bluear = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    starter = 14 - (((valmax - divav) / 16) - 1) # Used to determine starting shade number
+
+    # This section assigns the original color to the starting shade array
+    redar[starter] = rgb[0]
+    greenar[starter] = rgb[1]
+    bluear[starter] = rgb[2]
+
+    down = (divav / 16) - 1 # Loop control variable for the following loop
+
+    # Loop that subtracts 16 from an RGB value (as long as the current value is greater than or equal
+    # to 16) and assigns it to the next position in the shade array
+    while (int(down) >= 0):
+        if int(redar[down + 1]) >= 16:
+            redar[down] = int(redar[down + 1]) - 16
+        else:
+            redar[down] = 0
+
+        if int(greenar[down + 1]) >= 16:
+            greenar[down] = int(greenar[down + 1]) - 16
+        else:
+            greenar[down] = 0
+
+        if int(bluear[down + 1]) >= 16:
+            bluear[down] = int(bluear[down + 1]) - 16
+        else:
+            bluear[down] = 0
+
+        down = int(down) - 1 # Increment LCV
+
+    controller = starter # Loop control variable for the following loop
+
+    # Loop that adds 16 from an RGB value (as long as the current value is less than or equal
+    # to 236) and assigns it to the next position in the shade array
+    while (int(controller) + 1 <= 15):
+        if int(redar[controller]) <= 239:
+            redar[controller + 1] = int(redar[controller]) + 16
+        else:
+            redar[controller + 1] = 255
+
+        if int(greenar[controller]) <= 239:
+            greenar[controller + 1] = int(greenar[controller]) + 16
+        else:
+            greenar[controller + 1] = 255
+
+        if int(bluear[controller]) <= 239:
+            bluear[controller + 1] = int(bluear[controller]) + 16
+        else:
+            bluear[controller + 1] = 255
+
+        controller = int(controller) + 1 # Increment LCV
+
+    counter = 0 # Loop control variable for the following loop
+
+    # Prints all 16 color shades from the shade array and ensures that they all have leading
+    # zeros if their length is less than 3
+    while (counter < 16):
+        print str(redar[counter]).zfill(3), str(greenar[counter]).zfill(3), str(bluear[counter]).zfill(3)
+        counter = counter + 1 # Increment LCV
+
 
 def main():
     rgb = [0, 0, 0] # Creates the rgb array in the main function's scope
 
-    count = flarg(sys.argv[1:], rgb)
+    count = flarg(sys.argv[1:], rgb) # Assigns a value to count from the flarg function
+
+    # If no value was assigned to count (via the --count flag), this statement executes the
+    # shades function (which uses rgb values passed on run-time
+    if int(count) == 0:
+        shades(rgb)
 
 if __name__ == "__main__":
     main()
