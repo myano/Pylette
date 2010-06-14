cce9575d94d7e1b1e1ca2e79a24a3161b4997411
Nathan Chowning <nathan@nathanchowning.com>
2010-06-14 00:23:11 -0400

    Fixed indention issues

--- pylette.py
+++ pylette.py
@@ -10,28 +10,28 @@ import getopt
 
 # --help function
 def usage():
-	print "Quit being a faggot"
+    print "Quit being a faggot"
 
 def main(argv):
-	count = 0 # Number of colors to use
-	try:
-		opts, args = getopt.getopt(argv, "hc", ["help", "count="])
+    count = 0 # Number of colors to use
+    try:
+        opts, args = getopt.getopt(argv, "hc", ["help", "count="])
 
 # Flag/argument error handling
-	except getopt.GetoptError:
-		print "You have specified an incorrect flag/value!"
-		usage()
-		sys.exit(2)
+    except getopt.GetoptError:
+        print "You have specified an incorrect flag/value!"
+        usage()
+        sys.exit(2)
 
 # Check for flags.  If flags aren't present, count remains at 0
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			usage()
-			sys.exit()
-		elif opt in ("-c", "--count"):
-			count = arg
-	source = "".join(args)
-	print count
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-c", "--count"):
+            count = arg
+        source = "".join(args)
+        print count
 
 if __name__ == "__main__":
-	main(sys.argv[1:]) # removes sys.argv[0] (Script name) since that's garbage
+    main(sys.argv[1:]) # removes sys.argv[0] (Script name) since that's garbage
