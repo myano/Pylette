e9cd6562e8a72537e7f5693a2a351bc56b4a17d6
Nathan Chowning <nathan@nathanchowning.com>
2010-06-14 01:39:14 -0400

    Argument and flag support complete

--- pylette.py
+++ pylette.py
@@ -9,7 +9,8 @@ import sys
 import getopt
 
 # This function checks for flags/arguments that have been passed via command line
-def flarg(argv):
+# If no flags were passed, it assigns three arguments to rgb[]
+def flarg(argv, rgb):
     count = 0 # Number of colors to use
     try:
         opts, args = getopt.getopt(argv, "hc", ["help", "count="])
@@ -28,14 +29,32 @@ def flarg(argv):
         elif opt in ("-c", "--count"):
             count = arg
     source = "".join(args)
-    print count
+
+    # Ensures that count is 16 or less.
+    if int(count) > 16:
+        print "Pylette only supports Palettes with 256 colors.  You have specified a larger palette!"
+        usage()
+        sys.exit(2)
+
+    if len(sys.argv) < 2:
+        print "YOU ENTERED NOTHING!"
+
+    # If count is still 0, arguments have been supplied.  Assign them.
+    if len(sys.argv) > 1 and int(count) == 0:
+        rgb[0] = sys.argv[1]
+        rgb[1] = sys.argv[2]
+        rgb[2] = sys.argv[3]
+
+    return count
 
 # --help function
 def usage():
     print "Quit being a faggot"
 
 def main():
-    flarg(sys.argv[1:]) # removes sys.argv[0] (Script name) since that's garbage
+    rgb = [0, 0, 0] # Creates the rgb array in the main function's scope
+
+    count = flarg(sys.argv[1:], rgb)
 
 if __name__ == "__main__":
     main()
